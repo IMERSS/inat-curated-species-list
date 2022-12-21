83fa082b571c639591ae639d544e0d240c4cbf41
Ben Keen <ben.keen@gmail.com>
2022-12-20 18:25:45 -0800

    curated user list updated

--- src/constants.js
+++ src/constants.js
@@ -2,11 +2,11 @@
  * These are the default values that appear in the interface after running `npm run start`. They're also used by the
  * script to generate the static results, when running `node generate.js`.
  */
-export const USERS = 'dave328,gpohl,mothmaniac,crispinguppy,neoarctica,jasondombroskie';
+export const USERS = 'dave328,gpohl,crispinguppy';
 export const TAXON_ID = '47157';
 export const PLACE_ID = '7085';
 export const GENERATED_FILENAME_FOLDER = './dist';
 export const GENERATED_FILENAME = 'data.json';
 
 // example: https://sisyphean.ca/inat/curated-bc-leps-list.json
-export const DATA_URL = ''; // enter URL of generated data source here (for standalone version)
+export const DATA_URL = 'https://sisyphean.ca/inat/curated-bc-leps-list.json'; // enter URL of generated data source here (for standalone version)
